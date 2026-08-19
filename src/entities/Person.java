b3abcc6d37a6e14d940fb019c51105f627e84058
Mohammed Al-Bulushi <Mo7balushi@gmail.com>
2026-08-19 09:01:45 +0400

    chore: init repo + Java .gitignorefeat(entities): Person attributes + full constructor [Task 1.1]

--- src/entities/Person.java
+++ src/entities/Person.java
@@ -2,4 +2,41 @@ package entities;
 
 public class Person {
 
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private String nationalId;
+    private int age;
+    private boolean active;
+
+    public Person(
+            String id,
+            String firstName,
+            String lastName,
+            String dateOfBirth,
+            String gender,
+            String phoneNumber,
+            String email,
+            String address,
+            String nationalId,
+            int age,
+            boolean active) {
+
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+        this.nationalId = nationalId;
+        this.age = age;
+        this.active = active;
+    }
 }
